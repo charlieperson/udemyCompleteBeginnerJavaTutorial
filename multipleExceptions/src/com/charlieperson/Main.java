61cb9df974f8cff9ba808b4f6f972322c63e0a82
Charlie Person <charlie.person@hyperisland.se>
2017-02-16 22:34:49 -0600

    Playing around with handeling parent exceptions for all sub-exceptions as well as the parents

--- multipleExceptions/src/com/charlieperson/Main.java
+++ multipleExceptions/src/com/charlieperson/Main.java
@@ -5,12 +5,14 @@ import java.text.ParseException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	    Test test = new Test();
 	    try {
             test.run();
         } catch(ParseException | IOException ex){
             System.out.println(ex.getMessage());
         }
+
+        test.input();
     }
 }
--- multipleExceptions/src/com/charlieperson/Test.java
+++ multipleExceptions/src/com/charlieperson/Test.java
@@ -1,10 +1,16 @@
 package com.charlieperson;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 
 public class Test {
     public void run() throws IOException, ParseException {
         throw new ParseException("Throwing new parse exception from run method in Test", 2);
+    }
+
+    public void input() throws FileNotFoundException, IOException{
+        throw new FileNotFoundException("Sorry mister- couldn't find your file.");
     }
 }
